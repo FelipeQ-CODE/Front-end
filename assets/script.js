b31819f1c53da0e94d90f73fe9d19582cf69ef0f
Felipe Quirino <felipe.wi23.fq@gmail.com>
2022-06-11 23:29:10 -0300

    att fundo

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+function exibirAlerta(){
+    alert("Seja bem-vindo!");
+
+}
+
+var cidade ="São Paulo";
+function mudarCidade(){
+    cidade="Porto alegre";
+}
+function minhaCidade{
+    alert('Minha Cidade que Nasci é' + cidade);
+}
+function selecionarCor(){
+    outrasCores();
+  }
+  
